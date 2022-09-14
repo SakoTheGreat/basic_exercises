f436bdc50e7a28968ed363836caaecec212c2b5d
Andrei Sakovich <sakowlandrei@mail.ru>
2022-09-14 23:14:37 +0200

    'domashka'

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -12,7 +12,15 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
+povtor9ki = {}
+for i in students:
+    for j, c in i.items():
+        povtor9ki[c] = povtor9ki.get(c, 0)+1
+
+for i, j in povtor9ki.items():
+    print(f'{i}: {j}')
+
+
 
 
 # Задание 2
@@ -26,7 +34,12 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
+maximalka = {}
+for i in students:
+    for j, c in i.items():
+        maximalka[c] = maximalka.get(c, 0)+1
+
+print(max(maximalka, key=lambda x: maximalka[x]))
 
 
 # Задание 3
@@ -51,8 +64,13 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
-
+nu_maksimalka = {}
+for spicochek_spiskov in school_students:
+    for prost_spisok in spicochek_spiskov:
+        for key, ne_pomny in prost_spisok.items():
+            nu_maksimalka[ne_pomny] = nu_maksimalka.get(ne_pomny, 0)+1
+    print(max(nu_maksimalka, key=lambda x: nu_maksimalka[x]))
+    nu_maksimalka = {}
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
@@ -72,7 +90,21 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
+dev_o4ko = 0
+mal_o4ko = 0
+clasiki = ''
+for spicki in school:
+    for key, value_vspomnil in spicki.items():
+        if key == 'class':
+            clasiki = value_vspomnil
+        else:
+            for sosiska_v_spiske in value_vspomnil:
+                if is_male[sosiska_v_spiske['first_name']]:
+                    mal_o4ko += 1
+                else:
+                    dev_o4ko += 1
+    print(f'Класс {clasiki}: девочки {dev_o4ko}, мальчики {mal_o4ko}')
+    dev_o4ko, mal_o4ko, clasiki = 0, 0, ''
 
 
 # Задание 5
@@ -91,5 +123,33 @@ is_male = {
     'Олег': True,
     'Миша': True,
 }
-# ???
+girls = 0
+boys = 0
+class_number = ''
+all_classes = {}
+list_for_all = []
+# Ну горит душа просто сделать принт с ответом, но сдерживаюсь=)
+for spicki in school:
+    for key, value_vspomnil in spicki.items():
+        if key == 'class':
+            class_number = value_vspomnil
+        else:
+            for sosiska_v_spiske in value_vspomnil:
+                if is_male[sosiska_v_spiske['first_name']]:
+                    boys += 1
+                else:
+                    girls += 1
+    all_classes[class_number] = all_classes.get(class_number, {'девочек': girls, 'мальчиков': boys})
+    list_for_all.append(all_classes)
+    girls, boys, class_number, all_classes = 0, 0, '', {}
+
+if list_for_all[0]['2a']['девочек']>list_for_all[1]['3c']['девочек']:
+    print(f'Больше всего девочек в классе 2a')
+else:
+    print(f'Больше всего девочек в классе 3c')
+
+if list_for_all[0]['2a']['мальчиков']>list_for_all[1]['3c']['мальчиков']:
+    print(f'Больше всего мальчиков в классе 2a')
+else:
+    print(f'Больше всего мальчиков в классе 3c')   
 
--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,36 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
+print(word[-1])
 
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
+print(word.lower().count('а'))
 
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-# ???
+glasnie_takie_klasnie = 'аеёиоуыэюя'
+counter = 0
+for i in word.lower():
+    if i in glasnie_takie_klasnie:
+        counter += 1
+print(counter)
 
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
+print(len(sentence.split()))
 
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+for i in sentence.split():
+    print(i[0])
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+# Ну как то я странно понимаю это условие, но ладно
+dlina = list(map(lambda x: len(x), sentence.split()))
+print(sum(dlina)/len(sentence.split()))
